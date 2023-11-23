2a4b630e1a6905d5e4e37b693ee363cf049893cd
Joonas Ervald <joonas.ervald@gmail.com>
2023-11-23 15:48:04 +0200

    Upload version 1.3.0

--- Package.swift
+++ Package.swift
@@ -3,31 +3,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "ComoduleSDK",
+    name: "ComoduleKit",
     platforms: [
         .iOS(.v15)
     ],
     products: [
         .library(
-            name: "ComoduleSDK",
-            targets: ["ComoduleSDK"]
+            name: "ComoduleKit",
+            targets: ["ComoduleKit"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/NordicSemiconductor/IOS-DFU-Library", .exact("4.13.0"))
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "ComoduleKit",
-			url: "https://github.com/Comodule/comodule-sdk-ios/releases/download/v1.0.0/ComoduleKit_v1.0.0.xcframework.zip",
-			checksum: "714f68331489b1539210f41900c8b1375fdd11fd9f06512588f9efb73b2d0399"
-        ),
-        .target(
-            name: "ComoduleSDK",
-            dependencies: [
-				"ComoduleKit",
-				.product(name: "NordicDFU", package: "IOS-DFU-Library")
-			]
+			url: "https://github.com/Comodule/comodule-sdk-ios/releases/download/v1.3.0/ComoduleKit_v1.3.0.xcframework.zip",
+			checksum: "67368cdb58a5374a690dd5547a167dafe16f41b50d7bdbb44d3ff40ccba34851"
         )
     ]
 )
